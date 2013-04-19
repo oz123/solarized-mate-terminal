a6698571a11b2f689985787f2e1a84ecd0c2de59
oz123 <nahumoz@gmail.com>
2013-04-19 18:29:20 +0200

    fix the colors to match light theme

--- solarized-mate.sh
+++ solarized-mate.sh
@@ -3,13 +3,15 @@
 dconf write  /org/mate/terminal/profiles/default/use-theme-colors "false"
 
 COLORS=`cat colors/palette`
-BASE03=`cat colors/base03`
+BASE3=`cat colors/base3`
 BASE01=`cat colors/base01`
+BASE00=`cat colors/base00`
 
 # set palette
 dconf write /org/mate/terminal/profiles/default/palette \"$COLORS\"
 
 # set highlighted color to be different from foreground-color
 dconf write  /org/mate/terminal/profiles/default/bold-color-same-as-fg false
-dconf write /org/mate/terminal/profiles/default/background-color \"$BASE03\"
-dconf write /org/mate/terminal/profiles/default/foreground-color \"$BASE01\"
+dconf write /org/mate/terminal/profiles/default/background-color \"$BASE3\"
+dconf write /org/mate/terminal/profiles/default/foreground-color \"$BASE00\"
+dconf write /org/mate/terminal/profiles/default/bold_color \"$BASE01\"
