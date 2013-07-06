0c663d65527cbb3820db7f4931c7a087850f64ac
Grégoire Pineau <lyrixx@lyrixx.info>
2013-07-06 17:23:38 +0200

    update profile

--- solarized-mate-dark.sh
+++ solarized-mate-dark.sh
@@ -1,17 +1,28 @@
-#!/bin/bash 
+#!/bin/bash
 
-dconf write  /org/mate/terminal/profiles/default/use-theme-colors "false"
+PROFILE=${1:-"default"}
+
+read -p "Update profile: '$PROFILE', continue? [N/y]"
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Abort"
+
+    exit 1
+fi
 
 COLORS=`cat colors/palette`
 BASE03=`cat colors/base03`
 BASE1=`cat colors/base1`
 BASE0=`cat colors/base0`
 
+# Do not use theme color
+dconf write /org/mate/terminal/profiles/${PROFILE}/use-theme-colors "false"
+
 # set palette
-dconf write /org/mate/terminal/profiles/default/palette \"$COLORS\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/palette \"$COLORS\"
 
 # set highlighted color to be different from foreground-color
-dconf write  /org/mate/terminal/profiles/default/bold-color-same-as-fg false
-dconf write /org/mate/terminal/profiles/default/background-color \"$BASE03\"
-dconf write /org/mate/terminal/profiles/default/foreground-color \"$BASE0\"
-dconf write /org/mate/terminal/profiles/default/bold_color \"$BASE1\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/bold-color-same-as-fg false
+dconf write /org/mate/terminal/profiles/${PROFILE}/background-color \"$BASE03\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/foreground-color \"$BASE0\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/bold_color \"$BASE1\"
--- solarized-mate.sh
+++ solarized-mate.sh
@@ -1,17 +1,29 @@
-#!/bin/bash 
+#!/bin/bash
 
-dconf write  /org/mate/terminal/profiles/default/use-theme-colors "false"
+PROFILE=${1:-"default"}
+
+read -p "Update profile: '$PROFILE', continue? [N/y]"
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Abort"
+
+    exit 1
+fi
+
 
 COLORS=`cat colors/palette`
 BASE3=`cat colors/base3`
 BASE01=`cat colors/base01`
 BASE00=`cat colors/base00`
 
+# Do not use theme color
+
+dconf write /org/mate/terminal/profiles/${PROFILE}/use-theme-colors "false"
 # set palette
-dconf write /org/mate/terminal/profiles/default/palette \"$COLORS\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/palette \"$COLORS\"
 
 # set highlighted color to be different from foreground-color
-dconf write  /org/mate/terminal/profiles/default/bold-color-same-as-fg false
-dconf write /org/mate/terminal/profiles/default/background-color \"$BASE3\"
-dconf write /org/mate/terminal/profiles/default/foreground-color \"$BASE00\"
-dconf write /org/mate/terminal/profiles/default/bold_color \"$BASE01\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/bold-color-same-as-fg false
+dconf write /org/mate/terminal/profiles/${PROFILE}/background-color \"$BASE3\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/foreground-color \"$BASE00\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/bold_color \"$BASE01\"
